fc0f8c707bd48c562a64e02a30af3274ee3f0d27
Yusuf <kurakiorg@gmail.com>
2023-01-14 16:35:54 +0300

    Debug P Element

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".inp");
-const p = document.querySelector(".p");
+const p = document.querySelector(".ps");
 
 // console.log(Math.random()*100);
 
 const randoms = Math.random()*100;
 const randoma = Math.floor(randoms);
 
-
 console.log(randoms);
 console.log(randoma);
 console.log(btn);
 
 let attempts = 0;
+let pp = document.createElement("p");
 
 btn.addEventListener("click", function(){
     let inputValue = input.value;
-    console.log(inputValue);
     if(inputValue < randoma){
         attempts++;
-        console.log(attempts);
-        // p.innerHTML = ("yukarı" + " " + attempts);
-        p.innerHTML = `Yukarı hata sayınız: ${attempts}`
+        pp.textContent = `Yukarı hata sayınız: ${attempts}`
+        p.append(pp);
     }
     else if(inputValue > randoma){
         attempts++;
-        console.log(attempts);
-        // p.innerHTML = "aşağı";
-        p.innerHTML = `Aşağı hata sayınız: ${attempts}`
+        pp.textContent = `Aşağı hata sayınız: ${attempts}`
+        p.append(pp);
     }
     else{
-        p.innerHTML = `Tahmin doğru, toplam hata sayınız: ${attempts} `;
+        pp.textContent = `Tahmin doğru, toplam hata sayınız: ${attempts} `;
+        p.append(pp);
     }
 })
